2cd049a63edd073b6007f3dd61dea2fb4fcf710e
tjsiv <tjsivelsjr@gmail.com>
2024-03-23 00:59:09 -0700

    first commit mostly done

--- mercurySedan.js
+++ mercurySedan.js
@@ -1,6 +1,5 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
-
+const VehicleModule = require('./vehicle').Vehicle
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
@@ -12,14 +11,50 @@ console.log(v.make)
 
 
 //TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
+class Car extends VehicleModule{
+    constructor(make, model, year, color, mileage){
 
+        this.maximumPassengers = 5;
+        this.passengers = 0;
+        this.numberOfWheels = 4;
+        this.maximumSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+        super(make, model, year, color, mileage)
+    }
+    loadPassengers (num){
+        if (this.passenger < this.maxPassengers) {
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;               
+            } else {
+                console.log(this.model + " " + this.make + " doesn't have enough room.");
 
+            }
+        } else {
+            console.log(this.model + " " + this.make + " is full");
+        }
+    }
+    start(){
+        if (this.fuel > 0){
+            console.log("Vroom!! Engine on")
+            return this.start = true;
+        } else{
+            console.log("No fuel")
+            return this.start = false;
+        }
 
+    
+    }
+    serviceCheck(){
+        if (this.mileage > 30000){
+            this.scheduleService = true;
+            return this.scheduleService;
+        }
+        
 
-
-
-
-
+    }
+}
 
 
 
@@ -33,3 +68,17 @@ console.log(v.make)
 
 
 //Create at least two new instances of the Car class and test them here:
+let myCar = new Car('Mercury', 'sedan', '2024', 'Red', 10000)
+let myNewCar = new Car('Mercury', 'sedan', '2018', 'blue', 50000)
+myCar.start()
+myCar.loadPassenger(5)
+myCar.stop()
+myCar.serciveCheck()
+
+myNewCar.start()
+myNewCar.loadPassenger(4)
+myNewCar.stop()
+myNewCar.serciveCheck()
+
+console.log(myCar)
+console.log(myNewCar)
